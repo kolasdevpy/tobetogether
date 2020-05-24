60993bc495a76d89504717f3912deba331aef14b
kolasdevpy <kolas.dev.py@gmail.com>
2020-05-24 11:44:06 +0300

    add account rework

--- account/models.py
+++ account/models.py
@@ -5,5 +5,5 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
 
-    # def __str__(self):
-    #     return 'Profile for user {}'.format(self.user.username)
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
--- tobetogether/urls.py
+++ tobetogether/urls.py
@@ -1,20 +1,14 @@
-
-from django.contrib import admin
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-
 ]
 
-
-# from django.conf.urls import include, url
-# from django.contrib import admin
-
-# urlpatterns = [
-#     url(r'^admin/', include(admin.site.urls)),
-#     url(r'^account/', include('account.urls')),
-# ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
